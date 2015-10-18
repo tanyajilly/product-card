76ba9ca2c98dc9f27d5e093cf9ab165422813630
tanyajilly <justilluzia@gmail.com>
2015-10-18 23:09:56 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-;(function(){
+;(function($){
+	$(document).ready(function(){
+		// slider init
+		var slider = $('.ba-slider');
+		slider.slick({
+			dots: true
+		});
 
-})();
+		// qty up and down
+		var qtyField = $('.ba-product__qty-field'),
+			qtyUp = $('.ba-product__qty-up'),
+			qtyDown = $('.ba-product__qty-down');
+
+		qtyUp.on("click", function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			qtyField.val(++oldVal);
+		});
+		qtyDown.on("click", function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			qtyField.val(--oldVal === 0 ? 1 : oldVal);
+
+		});
+	});
+})(jQuery);
